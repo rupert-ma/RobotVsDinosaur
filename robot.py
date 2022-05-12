5e19cc7632fb7189b685b28199d52d6900835f6a
rupert-ma <michael.a.rupert@gmail.com>
2022-05-12 09:52:08 -0700

    added class attributes and start of functions

---
+++ robot.py
@@ -0,0 +1,11 @@
+from weapon import Weapon
+
+
+class Robot:
+    def __init__(self, name):
+        self.name = name
+        self.weapon = Weapon
+        self.health = 100
+
+    def attack(self, dinosaur):
+        pass
