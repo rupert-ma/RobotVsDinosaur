727463b79a09beb940c0e56d4dd65a16e879b1da
rupert-ma <michael.a.rupert@gmail.com>
2022-05-13 08:20:31 -0700

    adding user prompts

--- battlefield.py
+++ battlefield.py
@@ -10,11 +10,13 @@ def run_game(new_robot, new_dinosaur):
     while is_dead == False:
         if new_dinosaur.health > 0:
             new_dinosaur.attack(new_robot)
-            print('Robots new health is ' + str(new_robot.health))
+            print(f'{new_dinosaur.name} attacks {new_robot.name} leaving {str(new_robot.health)} hitpoints remaining')
         elif new_dinosaur.health == 0:
             is_dead = True
+            print(f'{new_robot.name} is the winner!')
         if new_robot.health > 0:
             new_robot.attack(new_dinosaur)
-            print('dinosaurs new health is ' + str(new_dinosaur.health))
+            print(f'{new_robot.name} attacks {new_dinosaur.name} leaving {str(new_dinosaur.health)} hitpoints remaining')
         elif new_robot.health == 0:
             is_dead = True
+            print(f'{new_dinosaur.name} is the winner!')
