2521f3ab8434c78a60dccf29a9eda7ac1e0ff059
rupert-ma <michael.a.rupert@gmail.com>
2022-05-12 22:04:02 -0700

    corrected while loop error

--- battlefield.py
+++ battlefield.py
@@ -1,5 +1,4 @@
-from robot import Robot
-from dinosaur import Dinosaur
+
 
 def run_game(new_robot, new_dinosaur):
     print('Dinosaurs starting health is:')
@@ -7,10 +6,15 @@ def run_game(new_robot, new_dinosaur):
     print('Robots starting health is:')
     print(new_robot.health)
 
-    while new_robot.health > 0 or new_dinosaur.health > 0:
+    is_dead = False
+    while is_dead == False:
         new_dinosaur.attack(new_robot)
         print('Robots new health is ' + str(new_robot.health))
 
         new_robot.attack(new_dinosaur)
         print('dinosaurs new health is ' + str(new_dinosaur.health))
 
+        if new_dinosaur.health == 0:
+            is_dead = True
+        if new_robot.health == 0:
+            is_dead = True
